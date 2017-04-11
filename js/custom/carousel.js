f1d71295cdab81cd022d8d11ea4c8ecc18ca97ed
msqgl <squeglia.michele@gmail.com>
2017-04-11 14:41:30 +0200

    aggiornato carousel

--- js/custom/carousel.js
+++ js/custom/carousel.js
@@ -1,10 +1,25 @@
 $(function () {
 
     $("#owl-example").owlCarousel({
+        items: 2,
         loop: true,
-        responsiveClass: true,
-        items: 1,
-        autoplay: true
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            }
+        }
     });
+    /*
+     $("#owl-example").owlCarousel({
+     loop: true,
+     responsiveClass: true,
+     items: 1,
+     autoplay: true
+     });
+     */
 
 });
